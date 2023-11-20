fd798509b1d338cb9d9a278cd7e60043f95f6bd1
Jongnnis <ilovepjh0608@gmail.com>
2023-11-20 15:14:58 +0900

    업로드 파일 저장

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ upload_folder = "c:\pjh\FastAPI"
 async def create_file(file: bytes | None = File(default=None)):
     if not file:
         return {"message": "파일이 존재하지 않음"}
-    return {'file_size': len(file)}
+    return {'file_size': len(file)} 
 
 # File()를 따로 생성할 필요가 없음
 # 만약 파일의 크기가 메모리 사이즈 이상을 넘어가면 디스크에 저장
